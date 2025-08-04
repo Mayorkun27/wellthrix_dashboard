34dfa8271eac424fabbaf7df62b484059d3887d7
Sanni Mubarak <oluwatoyinayomide6@gmail.com>
2025-08-04 16:22:33 +0100

    Profile

--- src/assets/assests.js
+++ src/assets/assests.js
@@ -1,4 +1,11 @@
 import logo from "./logo.png"
+import icon1 from "./icon1.png"
+import icon2 from "./icon2.png"
+import icon3 from "./icon3.png"
+import pic1 from "./pic1.png"
+import pic2 from "./pic2.png"
+import pic3 from "./pic3.png"
+import pic4 from "./pic4.png"
 
 const assets = {
     logo,
