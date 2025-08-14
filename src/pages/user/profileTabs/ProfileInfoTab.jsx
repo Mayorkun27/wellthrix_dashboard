08bef320646c76a0e4d2301eb562838440fd00f3
Sanni Mubarak <oluwatoyinayomide6@gmail.com>
2025-08-14 19:30:10 +0100

    full

--- src/pages/user/profileTabs/ProfileInfoTab.jsx
+++ src/pages/user/profileTabs/ProfileInfoTab.jsx
@@ -55,7 +55,10 @@ const ProfileInfoTab = () => {
           </div>
           <div className='w-full border-b-3 border-white flex justify-between items-center'>
             <p className='md:text-lg mb-2'><span className='font-semibold'>Package: </span>{user?.plan}</p>
-            <div className='flex gap-1 text-[#0F16D7] font-semibold items-center mb-2 md:text-lg'>
+            <Link
+              to="/upgradepackage"
+              className="flex gap-1 text-[#0F16D7] font-semibold items-center mb-2 md:text-lg"
+            >
               <p>Upgrade</p>
               <FaArrowUpLong />
             </Link>
